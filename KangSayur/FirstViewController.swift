d9c1b3b1133b487bb851d981241fa8b43f5f2fa0
Bimo Sekti Wicaksono <bimo.wicaksono@gramedia.digital>
2020-05-12 09:27:49 +0700

    First commit

---
+++ KangSayur/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  KangSayur
+//
+//  Created by Bimo Sekti Wicaksono on 12/05/20.
+//  Copyright © 2020 Team 11 MC2. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ KangSayur/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  KangSayur
+//
+//  Created by Bimo Sekti Wicaksono on 12/05/20.
+//  Copyright © 2020 Team 11 MC2. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
