f708d258f5cf49e2ae557f9a9a74d9e212075a61
Ajeng <salicca.rahajeng@gmail.com>
2020-05-25 17:37:59 +0700

    cart storyboard, cart cell+xib, asset + 1 gambar

--- KangSayur/Order/CartVC.swift
+++ KangSayur/Order/CartVC.swift
@@ -10,21 +10,39 @@ import UIKit
 
 class CartVC: UIViewController {
 
+    @IBOutlet weak var cartTable: UITableView!
+    @IBOutlet weak var imageTukangSayur: UIImageView!
+    @IBOutlet weak var lblTukangSayur: UILabel!
+    @IBOutlet weak var lblDaerahOperasi: UILabel!
+    @IBOutlet weak var lblNoTelp: UILabel!
+    @IBOutlet weak var lblTotalHarga: UILabel!
+    @IBOutlet weak var btnPesan: UIButton!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func btnPesanClicked(_ sender: UIButton) {
+        
     }
-    */
+    
+}
 
+extension CartVC: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cartTableCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
 }
