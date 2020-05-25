0cb3bed860a53b6772bddcb6f42a39d3ccfbd339
Jordan Andra Kurniawan <>
2020-05-25 16:12:51 +0700

    Profile & History

---
+++ KangSayur/Profile/EditProfileVC.swift
@@ -0,0 +1,21 @@
+//
+//  EditProfileVC.swift
+//  KangSayur
+//
+//  Created by Jordan Andra Kurniawan on 19/05/20.
+//  Copyright © 2020 Team 11 MC2. All rights reserved.
+//
+
+import SwiftUI
+
+struct EditProfileVC: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EditProfileVC_Previews: PreviewProvider {
+    static var previews: some View {
+        EditProfileVC()
+    }
+}
