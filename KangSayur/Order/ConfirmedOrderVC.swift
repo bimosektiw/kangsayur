767ee4bd7373596a4534b1e11273303c1014fce9
Ajeng <salicca.rahajeng@gmail.com>
2020-05-28 19:41:08 +0700

    fixing table view, working stepper with alert to delete row/array, populating with dummy data

---
+++ KangSayur/Order/ConfirmedOrderVC.swift
@@ -0,0 +1,34 @@
+//
+//  ConfirmedOrderVC.swift
+//  KangSayur
+//
+//  Created by Salicca Dewi Rahajeng on 26/05/20.
+//  Copyright © 2020 Team 11 MC2. All rights reserved.
+//
+
+import UIKit
+
+class ConfirmedOrderVC: UIViewController {
+
+    @IBOutlet weak var btnKembaliKeLangganan: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func btnKembaliKeLanggananClicked(_ sender: UIButton) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
